2661e715e1b04870712d902b5d859230f0f4f1e5
Mateusz <mateusz.grochala@yahoo.dk>
2017-02-16 19:34:29 +0100

    Optimized images and added more dummy data

--- src/app/shared/product-data/data.js
+++ src/app/shared/product-data/data.js
@@ -6,7 +6,7 @@ exports.PRODUCTDATA = [
         category: 'running shoe',
         price: 86.99,
         oldprice: 95.99,
-        description: 'God og robust terrænløbesko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod.',
+        description: 'God og robust terrænløbesko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum  Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
         rating: 4,
         options: [
             {
@@ -39,9 +39,9 @@ exports.PRODUCTDATA = [
                 ]
             }
         ],
-        heroimg: './app/assets/imgs/products/shoe.jpg',
-        img2: './app/assets/imgs/highlights/1.jpg',
-        img3: './app/assets/imgs/highlights/2.jpg',
+        heroimg: './app/assets/imgs/products/shoe1.jpg',
+        img2: './app/assets/imgs/products/shoe2.jpg',
+        img3: './app/assets/imgs/products/shoe3.jpg',
         brandimg: './app/assets/imgs/brand-logo.png'
     },
     {
@@ -49,7 +49,8 @@ exports.PRODUCTDATA = [
         name: 'G-Tx 6 Super',
         category: 'tennis shoe',
         price: 64.99,
-        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod.',
+        oldprice: 86.99,
+        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
         rating: 3,
         options: [
             {
@@ -75,9 +76,199 @@ exports.PRODUCTDATA = [
                 ]
             }
         ],
-        heroimg: './app/assets/imgs/products/shoe.jpg',
-        img2: './app/assets/imgs/products/shoe.jpg',
-        img3: './app/assets/imgs/products/shoe.jpg',
+        heroimg: './app/assets/imgs/products/shoe2.jpg',
+        img2: './app/assets/imgs/products/shoe3.jpg',
+        img3: './app/assets/imgs/products/shoe4.jpg',
+        brandimg: './app/assets/imgs/brand-logo.png'
+    },
+    {
+        id: 3,
+        name: 'GEL-FUJITRABUCO 5 G-Tx',
+        category: 'running shoe',
+        price: 86.99,
+        oldprice: 95.99,
+        description: 'God og robust terrænløbesko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum  Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
+        rating: 4,
+        options: [
+            {
+                color: 'Red',
+                sizes: [
+                    43,
+                    44,
+                    45,
+                    46
+                ]
+            },
+            {
+                color: 'Black',
+                sizes: [
+                    44,
+                    45,
+                    46,
+                    47,
+                    48
+                ]
+            },
+            {
+                color: 'Blue',
+                sizes: [
+                    41,
+                    42,
+                    43,
+                    44,
+                    45
+                ]
+            }
+        ],
+        heroimg: './app/assets/imgs/products/shoe3.jpg',
+        img2: './app/assets/imgs/products/shoe4.jpg',
+        img3: './app/assets/imgs/products/shoe5.jpg',
+        brandimg: './app/assets/imgs/brand-logo.png'
+    },
+    {
+        id: 4,
+        name: 'G-Tx 6 Super',
+        category: 'tennis shoe',
+        price: 64.99,
+        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
+        rating: 3,
+        options: [
+            {
+                color: 'White',
+                sizes: [
+                    38,
+                    39,
+                    40,
+                    41,
+                    42,
+                    43
+                ]
+            },
+            {
+                color: 'Black',
+                sizes: [
+                    40,
+                    41,
+                    42,
+                    43,
+                    44,
+                    45
+                ]
+            }
+        ],
+        heroimg: './app/assets/imgs/products/shoe4.jpg',
+        img2: './app/assets/imgs/products/shoe5.jpg',
+        img3: './app/assets/imgs/products/shoe1.jpg',
+        brandimg: './app/assets/imgs/brand-logo.png'
+    },
+    {
+        id: 5,
+        name: 'G-Tx 6 Super',
+        category: 'tennis shoe',
+        price: 74.99,
+        oldprice: 95.99,
+        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
+        rating: 3,
+        options: [
+            {
+                color: 'White',
+                sizes: [
+                    38,
+                    39,
+                    40,
+                    41,
+                    42,
+                    43
+                ]
+            },
+            {
+                color: 'Black',
+                sizes: [
+                    40,
+                    41,
+                    42,
+                    43,
+                    44,
+                    45
+                ]
+            }
+        ],
+        heroimg: './app/assets/imgs/products/shoe5.jpg',
+        img2: './app/assets/imgs/products/shoe1.jpg',
+        img3: './app/assets/imgs/products/shoe2.jpg',
+        brandimg: './app/assets/imgs/brand-logo.png'
+    },
+    {
+        id: 6,
+        name: 'G-Tx 6 Super',
+        category: 'tennis shoe',
+        price: 84.99,
+        oldprice: 95.99,
+        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
+        rating: 3,
+        options: [
+            {
+                color: 'White',
+                sizes: [
+                    38,
+                    39,
+                    40,
+                    41,
+                    42,
+                    43
+                ]
+            },
+            {
+                color: 'Black',
+                sizes: [
+                    40,
+                    41,
+                    42,
+                    43,
+                    44,
+                    45
+                ]
+            }
+        ],
+        heroimg: './app/assets/imgs/products/shoe4.jpg',
+        img2: './app/assets/imgs/products/shoe3.jpg',
+        img3: './app/assets/imgs/products/shoe2.jpg',
+        brandimg: './app/assets/imgs/brand-logo.png'
+    },
+    {
+        id: 7,
+        name: 'G-Tx 6 Super',
+        category: 'tennis shoe',
+        price: 64.99,
+        description: 'God og robust tennis sko med GORE-TEX-membran. Overdel i let mesh og sål med affjedring både i hæl og forfod. Lorem ipsum Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus.',
+        rating: 3,
+        options: [
+            {
+                color: 'White',
+                sizes: [
+                    38,
+                    39,
+                    40,
+                    41,
+                    42,
+                    43
+                ]
+            },
+            {
+                color: 'Black',
+                sizes: [
+                    40,
+                    41,
+                    42,
+                    43,
+                    44,
+                    45
+                ]
+            }
+        ],
+        heroimg: './app/assets/imgs/products/shoe1.jpg',
+        img2: './app/assets/imgs/products/shoe3.jpg',
+        img3: './app/assets/imgs/products/shoe5.jpg',
         brandimg: './app/assets/imgs/brand-logo.png'
     }
 ];
